e5d0cfc77309d7965a8271c79667c5d3b98a1c5b
Christian Glover <gloverc1015@gmail.com>
2024-06-20 00:55:29 -0400

    format

--- frontend/components/ScreenContent.tsx
+++ frontend/components/ScreenContent.tsx
@@ -4,11 +4,11 @@ import { Pressable, ScrollView, View } from 'react-native';
 
 import { Button } from './Button';
 import { LineGraph } from './LineGraph';
+import PollenView from './PollenView';
 import { Avatar } from './ui/avatar';
 import { Card } from './ui/card';
 import { Separator } from './ui/separator';
 import { H1 } from './ui/typography';
-import PollenView from './PollenView';
 
 type ScreenContentProps = {
   title: string;
@@ -55,7 +55,7 @@ export const ScreenContent = ({ title, path, children }: ScreenContentProps) => 
               </View>
             </View>
             <View className="w-1/2 flex-row justify-between pl-1">
-              <Card className="h-full w-full" >
+              <Card className="h-full w-full">
                 <PollenView />
               </Card>
             </View>
